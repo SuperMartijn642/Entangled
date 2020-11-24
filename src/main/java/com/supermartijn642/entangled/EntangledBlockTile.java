f7488b215859f3671680627df81ea7d80252637d
SuperMartijn624 <martijnvandenbrand@outlook.com>
2020-11-24 14:24:05 +0100

    check for null pointers when getting the dimension

--- src/main/java/com/supermartijn642/entangled/EntangledBlockTile.java
+++ src/main/java/com/supermartijn642/entangled/EntangledBlockTile.java
@@ -95,7 +95,7 @@ public class EntangledBlockTile extends TileEntity implements ITickableTileEntit
             if(this.world.isRemote && this.world.func_234923_W_() != this.dimension)
                 return LazyOptional.empty();
             World world = this.getDimension();
-            if(this.world != null){
+            if(world != null){
                 TileEntity tile = world.getTileEntity(this.pos);
                 if(checkTile(tile))
                     return tile.getCapability(capability);
