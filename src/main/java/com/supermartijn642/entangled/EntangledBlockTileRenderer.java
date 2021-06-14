f8eec659147becbe3b33849058e4167de2f71aef
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-06-14 18:44:40 +0200

    Properly fit large blocks inside the entangle block

--- src/main/java/com/supermartijn642/entangled/EntangledBlockTileRenderer.java
+++ src/main/java/com/supermartijn642/entangled/EntangledBlockTileRenderer.java
@@ -1,7 +1,6 @@
 package com.supermartijn642.entangled;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockRenderType;
 import net.minecraft.block.BlockState;
 import net.minecraft.client.Minecraft;
@@ -10,6 +9,8 @@ import net.minecraft.client.renderer.tileentity.TileEntityRenderer;
 import net.minecraft.client.renderer.tileentity.TileEntityRendererDispatcher;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.shapes.VoxelShape;
 import net.minecraft.util.math.vector.Quaternion;
 
 /**
@@ -24,36 +25,47 @@ public class EntangledBlockTileRenderer extends TileEntityRenderer<EntangledBloc
     }
 
     @Override
-    public void render(EntangledBlockTile tileEntityIn, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int combinedLightIn, int combinedOverlayIn){
-        if(!tileEntityIn.isBound())
+    public void render(EntangledBlockTile tile, float partialTicks, MatrixStack matrixStack, IRenderTypeBuffer buffer, int combinedLight, int combinedOverlay){
+        if(!tile.isBound())
             return;
 
-        Block boundBlock = tileEntityIn.getWorld().getDimensionKey() == tileEntityIn.getBoundDimension() ? tileEntityIn.getWorld().getBlockState(tileEntityIn.getBoundBlockPos()).getBlock() : null;
-        TileEntity boundTile = tileEntityIn.getWorld().getDimensionKey() == tileEntityIn.getBoundDimension() ? tileEntityIn.getWorld().getTileEntity(tileEntityIn.getBoundBlockPos()) : null;
-        BlockState state = tileEntityIn.getBoundBlockState();
+        TileEntity boundTile = tile.getWorld().getDimensionKey() == tile.getBoundDimension() ? tile.getWorld().getTileEntity(tile.getBoundBlockPos()) : null;
+        BlockState boundState = tile.getBoundBlockState();
 
-        matrixStackIn.push();
+        boolean renderTile = boundTile != null && canRenderTileEntity(boundTile.getType().getRegistryName());
+        boolean renderBlock = boundState != null && boundState.getRenderType() == BlockRenderType.MODEL && canRenderBlock(boundState.getBlock().getRegistryName());
 
-        matrixStackIn.translate(0.5, 0.5, 0.5);
+        // get the bounding box
+        AxisAlignedBB bounds = new AxisAlignedBB(0, 0, 0, 1, 1, 1);
+        if(renderBlock && tile.getWorld().getDimensionKey() == tile.getBoundDimension()){
+            VoxelShape shape = boundState.getRenderShape(tile.getWorld(), tile.getBoundBlockPos());
+            if(!shape.isEmpty())
+                bounds = shape.getBoundingBox();
+        }
+
+        matrixStack.push();
+
+        // rotate and scale
+        matrixStack.translate(0.5, 0.5, 0.5);
         float angleX = System.currentTimeMillis() % 10000 / 10000f * 360f;
         float angleY = System.currentTimeMillis() % 11000 / 11000f * 360f;
         float angleZ = System.currentTimeMillis() % 12000 / 12000f * 360f;
-        matrixStackIn.rotate(new Quaternion(angleX, angleY, angleZ, true));
+        matrixStack.rotate(new Quaternion(angleX, angleY, angleZ, true));
+        float scale = 0.4763f / (float)Math.sqrt((bounds.getXSize() * bounds.getXSize() + bounds.getYSize() * bounds.getYSize() + bounds.getZSize() * bounds.getZSize()) / 4);
+        matrixStack.scale(scale, scale, scale);
+        matrixStack.translate(-bounds.getXSize() / 2, -bounds.getYSize() / 2, -bounds.getZSize() / 2);
 
-        matrixStackIn.scale(0.55f, 0.55f, 0.55f);
-        matrixStackIn.translate(-0.5, -0.5, -0.5);
-
-        if(boundBlock != null && boundTile != null && canRenderTileEntity(boundBlock.getRegistryName())){
+        if(renderTile){
             if(!(boundTile instanceof EntangledBlockTile) || depth < 10){
                 depth++;
-                TileEntityRendererDispatcher.instance.renderTileEntity(boundTile, partialTicks, matrixStackIn, bufferIn);
+                TileEntityRendererDispatcher.instance.renderTileEntity(boundTile, partialTicks, matrixStack, buffer);
                 depth--;
             }
         }
-        if(state != null && state.getRenderType() == BlockRenderType.MODEL && canRenderBlock(state.getBlock().getRegistryName()))
-            Minecraft.getInstance().getBlockRendererDispatcher().renderBlock(state, matrixStackIn, bufferIn, combinedLightIn, combinedOverlayIn);
+        if(renderBlock)
+            Minecraft.getInstance().getBlockRendererDispatcher().renderBlock(boundState, matrixStack, buffer, combinedLight, combinedOverlay);
 
-        matrixStackIn.pop();
+        matrixStack.pop();
     }
 
     private static boolean canRenderBlock(ResourceLocation block){
