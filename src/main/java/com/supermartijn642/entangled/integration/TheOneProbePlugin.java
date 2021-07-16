1bb35531f2d529def7878160972ee6c47ea2c5c7
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-07-16 16:36:49 +0200

    Fix The One Probe support

--- src/main/java/com/supermartijn642/entangled/integration/TheOneProbePlugin.java
+++ src/main/java/com/supermartijn642/entangled/integration/TheOneProbePlugin.java
@@ -7,6 +7,7 @@ import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.InterModComms;
 import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
@@ -19,7 +20,7 @@ import java.util.function.Function;
 public class TheOneProbePlugin {
 
     public static void interModEnqueue(InterModEnqueueEvent e){
-        InterModComms.sendTo("theoneprobe", "getTheOneProbe", ProbeInfoProvider::new);
+        InterModComms.sendTo("theoneprobe", "getTheOneProbe", () -> new ProbeInfoProvider()); // Don't use a lambda, it crashes things
     }
 
     public static class ProbeInfoProvider implements IProbeInfoProvider, Function<ITheOneProbe,Void> {
@@ -41,16 +42,16 @@ public class TheOneProbePlugin {
             if(tile instanceof EntangledBlockTile){
                 if(((EntangledBlockTile)tile).isBound()){
                     BlockState boundBlockState = ((EntangledBlockTile)tile).getBoundBlockState();
-                    String boundBlock = TextStyleClass.HIGHLIGHTED + (boundBlockState == null ? "Block" : TextComponents.blockState(boundBlockState).format()) + TextStyleClass.INFO;
+                    ITextComponent boundBlock = TextComponents.string(TextStyleClass.HIGHLIGHTED.toString()).append(boundBlockState == null ? TextComponents.string("Block").get() : TextComponents.blockState(boundBlockState).get()).append(TextComponents.string(TextStyleClass.INFO.toString()).get()).get();
                     BlockPos boundPos = ((EntangledBlockTile)tile).getBoundBlockPos();
                     String x = TextStyleClass.HIGHLIGHTED.toString() + boundPos.getX() + TextStyleClass.INFO;
                     String y = TextStyleClass.HIGHLIGHTED.toString() + boundPos.getY() + TextStyleClass.INFO;
                     String z = TextStyleClass.HIGHLIGHTED.toString() + boundPos.getZ() + TextStyleClass.INFO;
-                    if(((EntangledBlockTile)tile).getBoundDimension() == world.getDimensionKey())
+                    if(((EntangledBlockTile)tile).getBoundDimension() == world.getDimensionKey()){
                         probeInfo.vertical().text(TextComponents.translation("entangled.waila.bound_same_dimension", boundBlock, x, y, z).get());
-                    else{
+                    }else{
                         String dimension = TextStyleClass.HIGHLIGHTED + TextComponents.dimension(((EntangledBlockTile)tile).getBoundDimension()).format() + TextStyleClass.INFO;
-                        probeInfo.text(TextComponents.translation("entangled.waila.bound_other_dimension", boundBlock, x, y, z, dimension).format());
+                        probeInfo.vertical().text(TextComponents.translation("entangled.waila.bound_other_dimension", boundBlock, x, y, z, dimension).get());
                     }
                 }else
                     probeInfo.text(TextComponents.translation("entangled.waila.unbound").get());
