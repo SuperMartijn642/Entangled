e568c11fd40ab1098ded2c866aa047a970a666ea
SuperMartijn624 <martijnvandenbrand@outlook.com>
2021-11-13 19:14:38 +0100

    update version

--- src/main/java/com/supermartijn642/entangled/Entangled.java
+++ src/main/java/com/supermartijn642/entangled/Entangled.java
@@ -20,8 +20,8 @@ public class Entangled {
 
     public static final String MODID = "entangled";
     public static final String NAME = "Entangled";
-    public static final String VERSION = "1.3.8";
-    public static final String DEPENDENCIES = "required-after:supermartijn642configlib@[1.0.9,);required-after:supermartijn642corelib@[1.0.12,)";
+    public static final String VERSION = "1.3.9";
+    public static final String DEPENDENCIES = "required-after:supermartijn642configlib@[1.0.9,);required-after:supermartijn642corelib@[1.0.13,)";
 
     @GameRegistry.ObjectHolder(Entangled.MODID + ":block")
     public static EntangledBlock block;
