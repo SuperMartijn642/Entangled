1232843b8696d2ab9d0cbd84b3ca12de7945f011
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-09-03 22:59:50 +0200

    Add generators for all data and assets

---
+++ src/main/java/com/supermartijn642/entangled/generators/EntangledBlockStateGenerator.java
@@ -0,0 +1,22 @@
+package com.supermartijn642.entangled.generators;
+
+import com.supermartijn642.core.generator.BlockStateGenerator;
+import com.supermartijn642.core.generator.ResourceCache;
+import com.supermartijn642.entangled.Entangled;
+import com.supermartijn642.entangled.EntangledBlock;
+import net.minecraft.util.ResourceLocation;
+
+/**
+ * Created 01/09/2022 by SuperMartijn642
+ */
+public class EntangledBlockStateGenerator extends BlockStateGenerator {
+
+    public EntangledBlockStateGenerator(ResourceCache cache){
+        super("entangled", cache);
+    }
+
+    @Override
+    public void generate(){
+        this.blockState(Entangled.block).variantsForProperty(EntangledBlock.ON, (state, builder) -> builder.model(new ResourceLocation("entangled", "block/" + (state.get(EntangledBlock.ON) ? "on" : "off"))));
+    }
+}
