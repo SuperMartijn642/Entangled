42e43ac487cd23bb4afd52d3c18bf90211d378f8
SuperMartijn624 <martijnvandenbrand@outlook.com>
2022-09-07 15:37:49 +0200

    Use BlockBlockEntityType instead of vanilla class

--- src/main/java/com/supermartijn642/entangled/Entangled.java
+++ src/main/java/com/supermartijn642/entangled/Entangled.java
@@ -1,6 +1,7 @@
 package com.supermartijn642.entangled;
 
 import com.google.common.collect.Sets;
+import com.supermartijn642.core.block.BaseBlockEntityType;
 import com.supermartijn642.core.item.BaseBlockItem;
 import com.supermartijn642.core.item.CreativeItemGroup;
 import com.supermartijn642.core.item.ItemProperties;
@@ -10,7 +11,6 @@ import com.supermartijn642.core.registry.RegistryEntryAcceptor;
 import com.supermartijn642.entangled.generators.*;
 import com.supermartijn642.entangled.integration.TheOneProbePlugin;
 import net.minecraft.resources.ResourceLocation;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
@@ -24,7 +24,7 @@ public class Entangled {
     @RegistryEntryAcceptor(namespace = "entangled", identifier = "block", registry = RegistryEntryAcceptor.Registry.BLOCKS)
     public static EntangledBlock block;
     @RegistryEntryAcceptor(namespace = "entangled", identifier = "tile", registry = RegistryEntryAcceptor.Registry.BLOCK_ENTITY_TYPES)
-    public static BlockEntityType<EntangledBlockEntity> tile;
+    public static BaseBlockEntityType<EntangledBlockEntity> tile;
     @RegistryEntryAcceptor(namespace = "entangled", identifier = "item", registry = RegistryEntryAcceptor.Registry.ITEMS)
     public static EntangledBinderItem item;
 
@@ -43,7 +43,7 @@ public class Entangled {
         handler.registerBlock("block", EntangledBlock::new);
         handler.registerItem("block", () -> new BaseBlockItem(block, ItemProperties.create().group(CreativeItemGroup.getDecoration())));
         // Entangled block entity type
-        handler.registerBlockEntityType("tile", () -> BlockEntityType.Builder.of(EntangledBlockEntity::new, block).build(null));
+        handler.registerBlockEntityType("tile", () -> BaseBlockEntityType.create(EntangledBlockEntity::new, block));
         // Entangled binder
         handler.registerItem("item", EntangledBinderItem::new);
     }
