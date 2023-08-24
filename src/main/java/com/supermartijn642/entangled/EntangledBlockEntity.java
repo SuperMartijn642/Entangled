7ae37a569c671dbcbd35efb2cb287a401cddfaf7
Campbell Cole <10430178+campbellcole@users.noreply.github.com>
2023-08-24 03:47:05 -0700

    Fix chunk loaded check in `EntangledBlockEntity` #66

--- src/main/java/com/supermartijn642/entangled/EntangledBlockEntity.java
+++ src/main/java/com/supermartijn642/entangled/EntangledBlockEntity.java
@@ -5,6 +5,7 @@ import com.supermartijn642.core.block.TickableBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.Registry;
+import net.minecraft.core.SectionPos;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
@@ -52,7 +53,7 @@ public class EntangledBlockEntity extends BaseBlockEntity implements TickableBlo
             return;
 
         boolean sendUpdate = false;
-        if(level.hasChunkAt(this.boundPos) || forceLoad){
+        if(forceLoad || chunkSource.getChunkNow(SectionPos.blockToSectionCoord(this.boundPos.getX()), SectionPos.blockToSectionCoord(this.boundPos.getZ())) != null){
             // Get the block and entity
             BlockState state = level.getBlockState(this.boundPos);
             BlockEntity entity = level.getBlockEntity(this.boundPos);
