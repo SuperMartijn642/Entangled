b1798049d542d9bf3d4bd70ccc05cb39c5d6e511
SuperMartijn624 <martijnvandenbrand@outlook.com>
2024-04-19 21:08:05 +0200

    Remove Applied Energistics and Refined Storage from block blacklist

--- src/main/java/com/supermartijn642/entangled/generators/EntangledTagGenerator.java
+++ src/main/java/com/supermartijn642/entangled/generators/EntangledTagGenerator.java
@@ -1,6 +1,5 @@
 package com.supermartijn642.entangled.generators;
 
-import com.supermartijn642.core.data.tag.entries.NamespaceTagEntry;
 import com.supermartijn642.core.generator.ResourceCache;
 import com.supermartijn642.core.generator.TagGenerator;
 import com.supermartijn642.entangled.Entangled;
@@ -21,9 +20,7 @@ public class EntangledTagGenerator extends TagGenerator {
         this.blockMineableWithPickaxe().add(Entangled.block);
 
         // Blacklist tags
-        this.blockTag(EntangledBlockEntity.BLACKLISTED_BLOCKS)
-            .addOptional(new NamespaceTagEntry("ae2"))
-            .addOptional(new NamespaceTagEntry("refinedstorage"));
+        this.blockTag(EntangledBlockEntity.BLACKLISTED_BLOCKS);
         this.blockTag(EntangledBlockEntityRenderer.BLACKLISTED_BLOCKS);
     }
 }
