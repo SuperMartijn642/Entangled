91792244a7062f826dcb68adbc676fd458e86494
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-01-16 02:11:33 +0100

    Automatically register api providers for entangled block

--- src/main/java/com/supermartijn642/entangled/EntangledBlockApiProviders.java
+++ src/main/java/com/supermartijn642/entangled/EntangledBlockApiProviders.java
@@ -1,34 +1,32 @@
 package com.supermartijn642.entangled;
 
-import com.supermartijn642.core.CommonUtils;
-import net.neoforged.fml.ModLoadingContext;
+import com.supermartijn642.entangled.extensions.BlockCapabilityExtension;
+import net.minecraft.resources.ResourceLocation;
 import net.neoforged.neoforge.capabilities.BlockCapability;
-import net.neoforged.neoforge.capabilities.Capabilities;
 import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent;
 
-import java.util.function.Consumer;
-import java.util.function.Supplier;
+import java.util.Set;
 
 /**
  * Created 21/03/2022 by SuperMartijn642
  */
 public class EntangledBlockApiProviders {
 
-    public static void register(){
-        tryRegisterApiProvider("neoforge", () -> () -> Capabilities.ItemHandler.BLOCK);
-        tryRegisterApiProvider("neoforge", () -> () -> Capabilities.FluidHandler.BLOCK);
-        tryRegisterApiProvider("neoforge", () -> () -> Capabilities.EnergyStorage.BLOCK);
+    public static void registerApiProviders(RegisterCapabilitiesEvent event, Set<BlockCapability<?,?>> capabilities){
+        for(BlockCapability<?,?> capability : capabilities){
+            //noinspection DataFlowIssue
+            if(isApiAllowed(((BlockCapabilityExtension)(Object)capability).entangled_getIdentifier()))
+                registerApiProvider(event, capability);
+        }
     }
 
-    private static void tryRegisterApiProvider(String modid, Supplier<Supplier<BlockCapability<?,?>>> api){
-        if(CommonUtils.isModLoaded(modid))
-            registerApiProvider(api.get().get());
+    private static <A, C> void registerApiProvider(RegisterCapabilitiesEvent event, BlockCapability<A,C> api){
+        event.registerBlockEntity(api, Entangled.tile, (entity, context) -> entity.getCapability(api, context));
     }
 
-    private static <A, C> void registerApiProvider(BlockCapability<A,C> api){
-        ModLoadingContext.get().getActiveContainer().getEventBus().addListener(
-            (Consumer<RegisterCapabilitiesEvent>)
-                event -> event.registerBlockEntity(api, Entangled.tile, (entity, context) -> entity.getCapability(api, context))
-        );
+    private static boolean isApiAllowed(ResourceLocation identifier){
+        if(identifier == null)
+            return false;
+        return !identifier.getNamespace().equals("ae2") && !identifier.getNamespace().equals("refinedstorage");
     }
 }
