110ad5808b7521110a606413690e6444f9a3783e
SuperMartijn624 <martijnvandenbrand@outlook.com>
2025-08-01 15:22:18 +0200

    Clear bound block entity when receiving server data #106

--- src/main/java/com/supermartijn642/entangled/EntangledBlockEntity.java
+++ src/main/java/com/supermartijn642/entangled/EntangledBlockEntity.java
@@ -329,6 +329,12 @@ public class EntangledBlockEntity extends BaseBlockEntity implements TickableBlo
                 this.directRedstoneSignal[index] = compound.getIntOr("directRedstoneSignal" + index, 0);
             }
             this.analogOutputSignal = compound.getIntOr("analogOutputSignal", 0);
+            // Clear bound block entity if it does not match the read dimension or position
+            if(this.boundBlockEntity != null && (this.boundBlockEntity.isRemoved()
+                || !this.boundBlockEntity.hasLevel()
+                || !this.boundPos.equals(this.boundBlockEntity.getBlockPos())
+                || !this.boundDimension.equals(this.boundBlockEntity.getLevel().dimension())))
+                this.boundBlockEntity = null;
         }
     }
 }
