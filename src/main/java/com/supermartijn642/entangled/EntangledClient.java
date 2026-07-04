67246f08ca2e186ee2c511dd7033d7a94da870a0
SuperMartijn624 <martijnvandenbrand@outlook.com>
2026-07-04 16:44:35 +0200

    Update to Minecraft 26.2

--- src/main/java/com/supermartijn642/entangled/EntangledClient.java
+++ src/main/java/com/supermartijn642/entangled/EntangledClient.java
@@ -8,7 +8,7 @@ import com.supermartijn642.core.registry.ClientRegistrationHandler;
 import com.supermartijn642.core.render.RenderUtils;
 import com.supermartijn642.core.render.RenderWorldEvent;
 import net.minecraft.client.multiplayer.ClientLevel;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.state.level.BlockOutlineRenderState;
 import net.minecraft.client.renderer.state.level.LevelRenderState;
 import net.minecraft.core.BlockPos;
@@ -54,31 +54,35 @@ public class EntangledClient {
             CompoundTag compound = stack.get(BaseBlock.TILE_DATA);
             if(compound.getBooleanOr("bound", false) && compound.getStringOr("dimension", "").equals(world.dimension().identifier().toString())){
                 BlockPos pos = new BlockPos(compound.getIntOr("boundx", 0), compound.getIntOr("boundy", 0), compound.getIntOr("boundz", 0));
+                BlockShape shape = BlockShape.create(world.getBlockState(pos).getOcclusionShape());
+                e.submitFeatures((poseStack, output) -> {
+                    poseStack.pushPose();
+                    Vec3 camera = RenderUtils.getCameraPosition();
+                    poseStack.translate(-camera.x, -camera.y, -camera.z);
+                    poseStack.translate(pos.getX(), pos.getY(), pos.getZ());
 
-                e.getPoseStack().pushPose();
-                Vec3 camera = RenderUtils.getCameraPosition();
-                e.getPoseStack().translate(-camera.x, -camera.y, -camera.z);
-                e.getPoseStack().translate(pos.getX(), pos.getY(), pos.getZ());
+                    RenderUtils.submitShape(output, poseStack, shape, 86 / 255f, 0 / 255f, 156 / 255f, 1, false);
+                    RenderUtils.submitShapeSides(output, poseStack, shape, 86 / 255f, 0 / 255f, 156 / 255f, 30 / 255f, false);
 
-                RenderUtils.renderShape(e.getPoseStack(), world.getBlockState(pos).getOcclusionShape(), 86 / 255f, 0 / 255f, 156 / 255f, false);
-                RenderUtils.renderShapeSides(e.getPoseStack(), world.getBlockState(pos).getOcclusionShape(), 86 / 255f, 0 / 255f, 156 / 255f, 30 / 255f, false);
-
-                e.getPoseStack().popPose();
+                    poseStack.popPose();
+                });
             }
         }else if(stack.getItem() == Entangled.item){
             EntangledBinderItem.BinderTarget target = stack.get(EntangledBinderItem.BINDER_TARGET);
             if(target != null && target.dimension().equals(world.dimension().identifier())){
                 BlockPos pos = target.pos();
+                BlockShape shape = BlockShape.create(world.getBlockState(pos).getOcclusionShape());
+                e.submitFeatures((poseStack, output) -> {
+                    poseStack.pushPose();
+                    Vec3 camera = RenderUtils.getCameraPosition();
+                    poseStack.translate(-camera.x, -camera.y, -camera.z);
+                    poseStack.translate(pos.getX(), pos.getY(), pos.getZ());
 
-                e.getPoseStack().pushPose();
-                Vec3 camera = RenderUtils.getCameraPosition();
-                e.getPoseStack().translate(-camera.x, -camera.y, -camera.z);
-                e.getPoseStack().translate(pos.getX(), pos.getY(), pos.getZ());
+                    RenderUtils.submitShape(output, poseStack, shape, 235 / 255f, 210 / 255f, 52 / 255f, 1, false);
+                    RenderUtils.submitShapeSides(output, poseStack, shape, 235 / 255f, 210 / 255f, 52 / 255f, 30 / 255f, false);
 
-                RenderUtils.renderShape(e.getPoseStack(), world.getBlockState(pos).getOcclusionShape(), 235 / 255f, 210 / 255f, 52 / 255f, false);
-                RenderUtils.renderShapeSides(e.getPoseStack(), world.getBlockState(pos).getOcclusionShape(), 235 / 255f, 210 / 255f, 52 / 255f, 30 / 255f, false);
-
-                e.getPoseStack().popPose();
+                    poseStack.popPose();
+                });
             }
         }
     }
@@ -109,7 +113,7 @@ public class EntangledClient {
         }
     }
 
-    private static boolean onBlockHighlightDraw(BlockOutlineRenderState outlineRenderState, MultiBufferSource.BufferSource bufferSource, PoseStack poseStack, boolean translucentPass, LevelRenderState levelRenderState){
+    private static boolean onBlockHighlightDraw(BlockOutlineRenderState outlineRenderState, SubmitNodeCollector output, PoseStack poseStack, LevelRenderState levelRenderState){
         BlockHighlightState state = levelRenderState.getRenderData(BLOCK_HIGHLIGHT_DATA);
         if(state == null || !state.shouldRender)
             return true;
@@ -119,8 +123,8 @@ public class EntangledClient {
         POSE_STACK.translate(-playerPos.x, -playerPos.y, -playerPos.z);
         POSE_STACK.translate(state.pos.getX(), state.pos.getY(), state.pos.getZ());
 
-        RenderUtils.renderShape(POSE_STACK, state.shape, 86 / 255f, 0 / 255f, 156 / 255f, false);
-        RenderUtils.renderShapeSides(POSE_STACK, state.shape, 86 / 255f, 0 / 255f, 156 / 255f, 30 / 255f, false);
+        RenderUtils.submitShape(output, POSE_STACK, state.shape, 86 / 255f, 0 / 255f, 156 / 255f, 1, false);
+        RenderUtils.submitShapeSides(output, POSE_STACK, state.shape, 86 / 255f, 0 / 255f, 156 / 255f, 30 / 255f, false);
 
         POSE_STACK.popPose();
         return true;
